3d693d4fa62bdbca8252b5a9b12fb7363b636fa3
Puosteh <vlad_teo2000@yahoo.com>
2017-09-16 15:17:09 +0300

    updated files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
+injectTapEventPlugin();
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <AppBar
+        title="Title"
+        iconClassNameRight="muidocs-icon-navigation-expand-more"
+        onLeftIconButtonTouchTap = {() => this.setState({open: !this.state.open})}
+        />
+        <Drawer
+           docked={false}
+           open={this.state.open}
+           onRequestChange={(open) => this.setState({open})}
+           >
+          <MenuItem>Menu Item</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+        </Drawer>
+        <BottomNavigation selectedIndex={this.state.selectedIndex}>
+          <div>
+          famelea mea
+          </div>
+        </BottomNavigation>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { MuiThemeProvider } from 'material-ui/styles';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<MuiThemeProvider> <App /> </MuiThemeProvider>, document.getElementById('root'));
+ReactDOM.render(<MuiThemeProvider muiThee={getMuiTheme(darkBaseTheme)}><App /></MuiThemeProvider>, document.getElementById('root'));
 registerServiceWorker();
